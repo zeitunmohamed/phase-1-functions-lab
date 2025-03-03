cc5a00d1f13b6166b83cc0523a8db7d0c768a4f4
zeitunmohamed <zeitunmohamed@gmail.com>
2025-03-03 14:28:01 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+//write your code here
+function distanceFromHqInBlocks(block) {
+  const hqBlock = 42;
+  return Math.abs(hqBlock - block);
+  }
+  function distanceFromHqInFeet(block) {
+  return distanceFromHqInBlocks(block) * 264;
+  }
+  function distanceTravelledInFeet(start, destination) {
+  return Math.abs(start - destination) * 264;
+  }
+  function calculatesFarePrice(start, destination) {
+  const feetTraveled = distanceTravelledInFeet(start, destination);
+  if (feetTraveled <= 400) {
+  return 0;
+  } else if (feetTraveled <= 2000) {
+  return (feetTraveled - 400) * 0.02; 
+  } else if (feetTraveled <= 2500) {
+  return 25; 
+  } else {
+  return 'cannot travel that far'; 
+  }
+  }
+  
+  module.exports = {
+  distanceFromHqInBlocks,
+  distanceFromHqInFeet,
+  distanceTravelledInFeet,
+  calculatesFarePrice
+  };
+  
+  
